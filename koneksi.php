22f3d40acb9f81575bd6843ccf5ef8662472016c
Reza Pahlevi <33834994+lowrenzer@users.noreply.github.com>
2019-12-27 23:19:32 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,16 @@
 <?php
-    $server = "localhost";
-    $user = "root";
-    $password = "";
-    $database = "db_mahasiswa";
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:lowappserver.database.windows.net,1433; Database = lowdb", "lowrenzer", "Rezareza1234");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
 
-    $koneksi = mysqli_connect($server, $user, $password, $database) or DIE ("Koneksi Gagal");
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "lowrenzer", "pwd" => "Rezareza1234", "Database" => "lowdb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:lowappserver.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
