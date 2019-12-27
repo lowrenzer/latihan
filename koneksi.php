5301bba3abfffa5eb61efe0a04eac8f901ba30e7
Reza Pahlevi <33834994+lowrenzer@users.noreply.github.com>
2019-12-27 21:03:29 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,8 @@
 <?php
-    $server = "localhost";
-    $user = "root";
-    $password = "";
-    $database = "db_mahasiswa";
+    $server = "lowappserver.database.windows.net";
+    $user = "lowrenzer";
+    $password = "Rezareza1234";
+    $database = "lowdb";
 
     $koneksi = mysqli_connect($server, $user, $password, $database) or DIE ("Koneksi Gagal");
 ?>
